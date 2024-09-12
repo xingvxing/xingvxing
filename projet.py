ddf44a4d59623a90a6fff4dcfda112457678c447
xingvxing <wang736217liu442@163.com>
2024-09-12 23:42:27 +0800

    Correction Pmeca

--- projet.py
+++ projet.py
@@ -8,7 +8,7 @@ def readlist(file) : return list(map(float,file.readline().split()))
 #%% Création des données
 
 VSST = 790 #Volts
-RSST = 33 #mOhm
+RSST = 33*1e-3 #mOhm
 RHOLAC = 131e-6 # Ohm/m
 RHORAIL = 18e-6 # Ohm/m
 
@@ -23,8 +23,8 @@ X = []
 
 while readedlist:
     Time, Position = readedlist
-    Times.append(Time/3600.) # On convertit en heure
-    X.append(Position*10**-3) # On convertit en km
+    Times.append(Time) # On convertit en heure
+    X.append(Position) # On convertit en km
     readedlist = readlist(file)
 
 Times = np.array(Times)
@@ -47,38 +47,40 @@ for i in range(len(X)-1):
 Acc = np.array(Acc)
 
 alpha = 0 # angle de la pente du chemin
-M = 70 #tonnes masse du train
+M = 70*1e3 #tonnes masse du train
 A0 = 780 #N constante forces
-A1 = 6.4 #N/tonnes constante accélération
+A1 = 6.4*1e-3 #N/tonnes constante accélération
 B0 = 0.0 # constante nulle ?
-B1 = 0.14 #N/tonnes/(km•h-1) constante
-C0 = 0.3634 #N/tonnes/(km•h-1)^2 constante inverse vitesse
+B1 = 0.14*3600/(1e3*1e3) #N/tonnes/(km•h-1) constante
+C0 = 0.3634*(3600**2)/(1e3*1e6) #N/tonnes/(km•h-1)^2 constante inverse vitesse
 C1 = 0.0
 
 FR = (A0 + A1*M) + (B0 + B1*M)*V + (C0 + C1*M)*V**2 # Force resistive
 
 Fm = M*Acc + M*9.81*np.sin(alpha) + FR # Force mécanique - ici alpha = 0
 
-Pm = Fm*V*10**-7
+Pm = Fm*V
 
 #%% Graphique
-fig, ax = plt.subplots(3, 1)
-ax[0].plot(Times*3600, X)
-ax[1].plot(Times*3600, V)
-ax[2].plot(Times*3600, Pm)
+fig, ax = plt.subplots(4, 1)
+ax[0].plot(Times, X)
+ax[1].plot(Times, V)
+ax[2].plot(Times, Pm)
+ax[3].plot(Times, Acc)
+
 
 ax[0].set_xlim([0,140])
 ax[1].set_xlim([0,140])
 ax[2].set_xlim([0,140])
 
-ax[0].set_ylim([0,1.5])
+# ax[0].set_ylim()
 
 ax[2].set_xlabel("t(s)")
 
 plt.show()
 
 #%% Partie électronique
-R1 = RSST*1e-3 + RHOLAC*X+RHORAIL*X # TODO à vérifier
+R1 = RSST + RHOLAC*X+RHORAIL*X # TODO à vérifier
 REQ = R1/2 # Car Somme de résistance en parallèle - 1/Req = 1/R1 + 1/R2 - TODO à vérifier
 Vtrain = 1./2. * (VSST + np.sqrt(VSST**2 - 4*REQ*(35*1e3)))# TODO à modifier
 # normalement pour aliment les différents systèmes à bord il faut une puissance constante et égale à 35kW donc PLAC = 35 kW ?
